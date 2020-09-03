75653a0b4bd7dfcac582d616dec13bdbac86ae10
Muhammad Magdi <muhammad.magdi@maxab.io>
2020-09-04 01:56:40 +0200

    move degree math methods into a separated package

---
+++ dmath/degree.math.go
@@ -0,0 +1,64 @@
+package dmath
+
+import "math"
+
+func ToRadian(d float64) float64 {
+	return float64(d) * math.Pi / 180.0
+}
+
+func ToDegree(r float64) float64 {
+	return r * 180.0 / math.Pi
+}
+
+func Sin(d float64) float64 {
+	return math.Sin(ToRadian(d))
+}
+
+func Cos(d float64) float64 {
+	return math.Cos(ToRadian(d))
+}
+
+func Tan(d float64) float64 {
+	return math.Tan(ToRadian(d))
+}
+
+func ASin(x float64) float64 {
+	return ToDegree(math.Asin(x))
+}
+
+func ACos(x float64) float64 {
+	return ToDegree(math.Acos(x))
+}
+
+func ATan(x float64) float64 {
+	return ToDegree(math.Atan(x))
+}
+
+func ACot(x float64) float64 {
+	return ToDegree(1.0 / math.Atan(1/x))
+}
+
+func ATan2(y, x float64) float64 {
+	return ToDegree(1.0 / math.Atan2(y, x))
+}
+
+func FixAngle(a float64) float64 {
+	return mod(a, 360)
+}
+
+func FixHour(a float64) float64 {
+	return mod(a, 24)
+}
+
+// mod returns a%b
+// Using the division algorithm a = q*b + r
+// Where q is floor(a/b), r is the remainder
+// Then r = a - q*b
+func mod(a float64, b float64) float64 {
+	q := math.Floor(a / b)
+	r := a - q*b
+	if r < 0 {
+		return b + r
+	}
+	return r
+}
