f068c6d71270e1963e89926745f1e620f49cf4cf
muhammad-magdi <muhammad.magdi1024@gmail.com>
2021-11-10 14:12:16 +0200

    prepare server framework

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/muhammad-magdi/islamic-calendar/prayers"
+)
 
 func main() {
-	fmt.Println("Hello")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	r := gin.Default()
+
+	prayersRouter := prayers.NewPrayersRouter()
+
+	r.Run(fmt.Sprintf(":%s", port))
 }
